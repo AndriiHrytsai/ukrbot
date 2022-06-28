e3c406b9485471161040583a184c1d777adeecdd
Andrii Hrytsai <andriyhrytsay@gmail.com>
2022-06-28 16:21:39 +0300

    first commit

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'static')));
+// const app = express();
+//
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'static')));
 
 // app.listen(3000, () => {
 //     console.log('App listen 3000');
